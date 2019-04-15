54dd419af43463f50b37860f3960131e21fa1cc9
vinaykr4321 <vinaykr4321@gmail.com>
2019-04-15 17:43:08 +0530

    Created todo_api with all functionality in routes.js file

---
+++ routes/todos.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+
+router.get('/', (req,res)=>{
+    db.Todo.find().then((todos)=>{
+        res.json(todos);
+    })
+    .catch((err)=>{
+        res.send(err);
+    })
+})
+
+
+router.post('/',(req,res)=>{
+    db.Todo.create(req.body)
+    .then((newTodo) =>{
+        res.status(201).json(newTodo);
+    })
+    .catch((err)=>{
+        res.send(err);
+    });
+});
+
+router.get('/:todoId',(req,res)=>{
+    db.Todo.findById(req.params.todoId)
+    .then(foundTodo =>{
+        res.json(foundTodo);
+    })
+    .catch(err => {
+        res.send(err);
+    })
+});
+
+router.put('/:todoId',(req,res) => {
+    db.Todo.findOneAndUpdate({_id:req.params.todoId}, req.body,{new : true})
+    .then((todo) => {
+        res.json(todo);
+    })
+    .catch(err=>{
+        res.send(err);
+    })
+});
+
+router.delete('/:todoId',(req,res)=>{
+    db.Todo.findOneAndDelete({_id:req.params.todoId})
+    .then((deletedTodo) =>{
+        res.json(deletedTodo);
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+module.exports = router;
